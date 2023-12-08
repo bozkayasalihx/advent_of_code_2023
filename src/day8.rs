ec5a1f3ae0a09eefe6564203dee81d3e5d3729db
bozkayasalihx <bozkayasalih01x@gmail.com>
2023-12-08 16:53:17 +0300

    added day 8 part 2 rust implementation

--- src/day8.rs
+++ src/day8.rs
@@ -1,5 +1,5 @@
 use std::collections::HashMap;
-use std::fs;
+use std::{fs, usize};
 
 fn main() {
     let input = fs::read_to_string("input.txt").expect("couldn't open the file");
@@ -25,22 +25,41 @@ fn main() {
         map_hash.insert(lhs, (x, y));
     }
 
-    let ans = part_one(&map_hash, insts);
+    // let ans = part_one(&map_hash, insts);
+    let ans = part_two(&map_hash, insts);
     println!("{ans}");
 }
 
-fn part_one(hash_data: &HashMap<&str, (&str, &str)>, insts: Vec<char>) -> u32 {
-    let mut counter = 0;
-    let mut current = "AAA";
+fn part_one(hash_data: &HashMap<&str, (&str, &str)>, insts: Vec<char>) -> usize {
+    let current = "AAA";
+    count_loop(hash_data, &insts, current)
+}
 
+fn part_two(hash_data: &HashMap<&str, (&str, &str)>, insts: Vec<char>) -> usize {
+    let count = hash_data
+        .keys()
+        .filter(|x| x.ends_with('A'))
+        .map(|x| count_loop(hash_data, &insts, x))
+        .reduce(|acc, cur| lcm(acc, cur))
+        .unwrap();
+
+    count
+}
+
+fn count_loop<'a>(
+    hash_data: &HashMap<&'a str, (&'a str, &'a str)>,
+    insts: &Vec<char>,
+    mut current: &'a str,
+) -> usize {
+    let mut counter = 0;
     'looper: loop {
-        for inst in &insts {
+        for inst in insts {
             match inst {
                 'R' => {
                     counter += 1;
-                    current = &hash_data.get(current).unwrap().1;
+                    current = hash_data.get(current).unwrap().1;
 
-                    if current == "ZZZ" {
+                    if current.chars().last() == Some('Z') {
                         break 'looper;
                     }
                 }
@@ -48,7 +67,7 @@ fn part_one(hash_data: &HashMap<&str, (&str, &str)>, insts: Vec<char>) -> u32 {
                     counter += 1;
                     current = &hash_data.get(current).unwrap().0;
 
-                    if current == "ZZZ" {
+                    if current.chars().last() == Some('Z') {
                         break 'looper;
                     }
                 }
@@ -58,4 +77,28 @@ fn part_one(hash_data: &HashMap<&str, (&str, &str)>, insts: Vec<char>) -> u32 {
     }
 
     counter
+}
+
+fn lcm(first: usize, second: usize) -> usize {
+    first * second / gcd(first, second)
+}
+
+fn gcd(first: usize, second: usize) -> usize {
+    let mut max = first;
+    let mut min = second;
+    if min > max {
+        let val = max;
+        max = min;
+        min = val;
+    }
+
+    loop {
+        let res = max % min;
+        if res == 0 {
+            return min;
+        }
+
+        max = min;
+        min = res;
+    }
 }
